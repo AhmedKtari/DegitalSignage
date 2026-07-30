f8705b300e0b0c988be3d6b18be4ce54452c8193
Ahmedktari <ahmedktari45@gmail.com>
2026-07-30 10:58:57 +0100

    base build

---
+++ src/main/java/io/github/ahmedktarii/degitalesingage/DegitaleSingageApplication.java
@@ -0,0 +1,13 @@
+package io.github.ahmedktarii.degitalesingage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DegitaleSingageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DegitaleSingageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/ahmedktarii/degitalesingage/DegitaleSingageApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ahmedktarii.degitalesingage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DegitaleSingageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
